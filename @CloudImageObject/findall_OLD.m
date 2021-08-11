ef262df910e902b8998b5b30b175bf16dcd586e8
BEC 5 <bec5five@gmail.com>
2021-08-11 09:45:02 -0400

    Cleanup deleted / renamed files

--- @CloudImageObject/findall_OLD.m
+++
@@ -1,9 +0,0 @@
-function objList = findall()
-    bigObjList=evalin('base','who');
-    objList={};
-    for i=1:length(bigObjList)
-        if strcmpi(class(evalin('base',bigObjList{i})),'CloudImageObject')
-            bigObjList{i};
-            objList{length(objList)+1}=evalin('base',bigObjList{i});
-        end
-    end
